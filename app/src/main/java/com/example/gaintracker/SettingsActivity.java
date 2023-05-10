568a661c57cc719307c1b92490f678ffa123b991
jakub <jakubmocek2002@gmail.com>
2023-05-10 16:18:51 +0200

    Pierwszy commit

---
+++ app/src/main/java/com/example/gaintracker/SettingsActivity.java
@@ -0,0 +1,25 @@
+package com.example.gaintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
